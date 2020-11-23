6ba8eb10e197687e4d99e509840ba6951b61d363
aubrey-heim <aubrey.l.heim@gmail.com>
2020-11-22 21:51:37 -0800

    added cascading delete

--- schema.sql
+++ schema.sql
@@ -3,15 +3,6 @@ CREATE DATABASE company_DB;
 
 USE company_DB;
 
-CREATE TABLE employee(
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT NULL,
-  PRIMARY KEY (id)
-);
-
 CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
@@ -23,6 +14,21 @@ CREATE TABLE role(
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
+  FOREIGN KEY (department_id)
+	REFERENCES department(id)
+    ON DELETE CASCADE,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
+  FOREIGN KEY (role_id)
+	REFERENCES role(id)
+    ON DELETE CASCADE,
   PRIMARY KEY (id)
 );
 
@@ -30,4 +36,4 @@ INSERT INTO department (name) values ("Student Success");
 
 INSERT INTO role (title, salary, department_id) values ("Dean", 150000, 1);
 
-INSERT INTO employee (first_name, last_name, role_id) values("John", "Doe", 1)
+INSERT INTO employee (first_name, last_name, role_id) values("John", "Doe", 1);
